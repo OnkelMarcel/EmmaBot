bcd3c3550aa025e35c5b05706f6e360f12e5afd5
OnkelMarcel <mrcelkrem30@yahoo.com>
2018-08-22 23:13:45 +0200

    Create StopCMD.java

---
+++ commands/StopCMD.java
@@ -0,0 +1,29 @@
+package emma.commands;
+
+import com.github.johnnyjayjay.discord.commandapi.CommandEvent;
+import com.github.johnnyjayjay.discord.commandapi.ICommand;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.util.concurrent.TimeUnit;
+
+public class StopCMD implements ICommand  {
+
+    @Override
+    public void onCommand(CommandEvent event, Member member, TextChannel channel, String[] args) {
+        if(event.getAuthor().isBot())return;
+
+        for(Permission perms : event.getMember().getPermissions()){
+            if(perms == Permission.ADMINISTRATOR || perms == Permission.KICK_MEMBERS || event.getAuthor().getId() == "209413133020823552"){
+                event.getChannel().sendMessage("Ich...Ich will noch nicht gehen, aber nichts hält mich noch hier....").queue();
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.exit(0);
+            }
+        }
+    }
+}
