ea9ddcdba7a0ae8b2d8e18ca5c7ec32dbad75a07
OnkelMarcel <mrcelkrem30@yahoo.com>
2018-08-22 23:12:50 +0200

    Create GoogleCMD.java

---
+++ commands/GoogleCMD.java
@@ -0,0 +1,20 @@
+package emma.commands;
+
+import com.github.johnnyjayjay.discord.commandapi.CommandEvent;
+import com.github.johnnyjayjay.discord.commandapi.ICommand;
+import emma.stuff.LMGTFY;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+public class GoogleCMD implements ICommand  {
+
+    @Override
+    public void onCommand(CommandEvent e, Member member, TextChannel channel, String[] args) {
+        if(e.getAuthor().isBot()) return;
+
+        LMGTFY lmgtfy = new LMGTFY(args, channel);
+        lmgtfy.search();
+
+    }
+
+}
